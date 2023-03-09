14ea2b6597d191dac68d9c29eac0c7d03f7a3f14
Laniakea <st.gino@gmail.com>
2023-03-09 18:26:29 +0800

    bubblesort1A

--- DSA/bubblesort.cpp
+++ DSA/bubblesort.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 using namespace std;
 
+
+//升序冒泡
 void bubbleSort(int A[],int n){//引入待排序列，和问题规模
         int sorted = false;
         while (!sorted)
         {
-            sorted = true;
-
-            for (int i = 1; i < n; i++)//
+            sorted = true;//假设已有序
+            for (int i = 1; i < n; i++)//一趟
             {
-                if (A[i-1]<A[i])//让i的前一个元素和递增的i位置元素依次比较
+
+                if (A[i-1]>A[i])//让i的前一个元素和递增i对应的位置元素依次比较，如果后面的小，那么互换
                 {
-                   // swap(A[i-1],A[i]);
-                    sorted = false;
+                    swap(A[i-1],A[i]);
+                    sorted = false;//出现局部逆序，假设毅然还未整体有序
                 } 
             }
-            n--;
+            n--;//，抛掉末尾已经就位的元素，问题规模-1
         }
     }
 
@@ -30,15 +32,21 @@ void bubbleSort(int A[],int n){//引入待排序列，和问题规模
 int main(void){
 
     cout<<"Reincarnation"<<endl;
-    int A[5]={3,7,5,2,6};
-    for (int i = 0; i <5; i++)
+    int A[5];
+
+    for (int i = 0; i <5; i++)  //循环输入数组
     {
-        cout<<A[i]<<endl;
+        cin>>A[i];
     }
-    bubbleSort(A,5);
-        for (int i = 0; i <5; i++)
+    for (int i = 0; i <5; i++)  //循环输出数组
     {
-        cout<<A[i];
+        cout<<A[i]<<",";
+    }
+    cout<<endl;
+    bubbleSort(A,5);
+    for (int i = 0; i <5; i++)  //循环输出排序结果
+    {
+        cout<<A[i]<<",";
     }
     
 cin.get();
