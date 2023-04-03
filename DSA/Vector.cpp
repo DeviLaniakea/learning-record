817173ce5f745ebbd03f8620b18110da4fc9dc5b
Laniakea <st.gino@gmail.com>
2023-04-03 23:36:20 +0800

    Class Vertor

--- DSA/Vector.cpp
+++ DSA/Vector.cpp
@@ -1,11 +1,13 @@
 typedef int Rank;   //秩
 #define DEFAULT_CAPACITY 3  //默认的初始容量（实际应用中可设置更大）
 
-template <typename T> class Vector  //向量模板类
+template <typename T> class Vector  //向量模板类，一个参数类型为T的叫做Vector的类的模板
 {
+    
 protected:
-    Rank_size; int _capacity; T* _elem;   //规模、容量、数据区
-    void copyFrom(T cons* A,Rank lo,Rank hi);    //复制数组区间A[lo, hi)
+
+    Rank _size; int _capacity; T* _elem;   //规模、容量、数据区
+    void copyFrom(T const* A,Rank lo,Rank hi);    //复制数组区间A[lo, hi)
     void expand();      //空间不足时扩容
     void shrink();  //装填因子过小时压缩
     void bubble(Rank lo,Rank hi);   //扫描交换
@@ -22,6 +24,10 @@ private:
     /* data */
 public:
     //构造函数
+    /*默认的构造方法是，
+    首先根据创建者指定的初始容量，向系统申请空间，以创建内部私有数组_elem[];
+    若容量未明确指定，则使用默认值DEFAULT_CAPACITY。
+    接下来，鉴于初生的向量尚不包含任何元素，故将指示规模的变量_size初始化为0。*/
     Vector(int c = DEFAULT_CAPACITY,int s = 0,T v = 0)  //容量为c，规模为s，所有元素初始为v
     {
         _elem = new T[_capacity = c];
@@ -62,4 +68,30 @@ public:
     void traverse ( void (* ) ( T& ) ); //遍历（使用函数指针，只读或局部性修改）
     template <typename VST> void traverse ( VST& ); //遍历（使用函数对象，可全局性修改）
 
+    
 };
+
+//基于复制的构造方法
+template <typename T> //元素类型
+void Vector<T>::copyFrom(T const* A, Rank lo, Rank hi) { //以数组区间A[lo, hi)为蓝本复制向量
+    _elem = new T[_capacity = 2 * ( hi - lo ) ]; _size = 0; //分配原数组2倍空间，规模清零 
+    while ( lo < hi ) //A[lo, hi)内元素逐一
+        _elem[_size++] = A[lo++]; //复制至_elem[0, hi - lo)
+}
+
+/*需强调的是，由于向量内部含有动态分配的空间，默认的运算符"="不足以支持向量之间的直接赋值。
+例如，6.3节将以二维向量形式实现图邻接表，其主向量中的每一元素本身都是一维向量，
+故通过默认赋值运算符，并不能复制向量内部的数据区。为适应此类赋值操作的需求，重载向量的赋值运算符。*/
+
+/*为什么要重载赋值运算符：对于简单的类，默认的赋值运算符一般就够用了，我们也没有必要再显式地重载它。
+但是当类持有其它资源时，例如动态分配的内存、打开的文件、指向其他数据的指针、网络连接等，
+默认的赋值运算符就不能处理了，我们必须显式地重载它，这样才能将原有对象的所有数据都赋值给新对象。*/
+
+/*运算符重载函数本质就是将一个运算符与一个类内函数做映射，运算符不再执行他原始的程序，而执行你重载那个函数*/
+template <typename T> 
+Vector<T>& Vector<T>::operator= ( Vector<T> const& V ) { //重载，不过是一个函数名比较特殊的一个函数，函数名规则是：operator接运算符
+    if ( _elem ) 
+        delete [] _elem; //释放原有内容
+    copyFrom ( V._elem, 0, V.size() ); //整体复制
+    return *this; //返回当前前对象的引用，以便链式赋值
+}
