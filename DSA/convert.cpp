5495352d5fc15093220ed66c6d1ffe8af8da8ac1
Laniakea <st.gino@gmail.com>
2023-05-18 18:00:34 +0800

    栈

--- DSA/convert.cpp
+++ DSA/convert.cpp
@@ -1,11 +1,24 @@
 #include "Stack.h"
+#include "iostream"
 
-//十进制到r进制转换：循环取余法
-void convert ( Stack<char>& S, __int64 n, int base ) { //十进制数n到base进制的转换（递归版）
+//十进制到r进制转换：循环取余法-递归版
+void convertA ( Stack<char>& S, __int64 n, int base ) { 
 static char digit[] //0 < n, 1 < base <= 16，新进制下的数位符号，可视base取值范围适当扩充
 = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 if ( 0 < n ) { //在尚有余数之前，不断通过递归得到所有更高位
-convert ( S, n / base, base ); 
-S.push ( digit[n % base] ); //输出低位
-}
+    S.push ( digit[n % base] ); 
+    convertA ( S, n / base, base ); 
+    }
+} //新进制下由高到低的各数位，自顶而下保存于栈S中
+
+
+//十进制数n到base进制的转换（迭代版）
+void convertB ( Stack<char>& S, __int64 n, int base ) { 
+static char digit[] //0 < n, 1 < base <= 16，新迕进制下单数位符号
+= { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+while ( n > 0 ) { //由低到高，逐一计算出新进制下的各数位
+    int remainder = ( int ) ( n % base ); 
+    S.push ( digit[remainder] ); //余数（当前位）入栈
+    n /= base; //n更新为其对base的除商
+    }
 } //新进制下由高到低的各数位，自顶而下保存于栈S中
